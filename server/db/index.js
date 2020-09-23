bfdf6c2d4cd3ef5c5171c3ebeff03641752cd3b5
Dzirt2006 <dzirt-2006@mail.ru>
2020-09-23 13:00:58 -0400

    some fixes

--- server/db/index.js
+++ server/db/index.js
@@ -1,12 +1,12 @@
 const Sequielize = require("sequelize");
-const package = require(".../.../package.json");
+const package = require("../../package.json");
 
 const dbName = package.name;
 
 
-const db = Sequielize(
+const db =new Sequielize(
     process.env.DATABASE_URL ||
-    `postgres://postgres:w2w2@localhost:5432/${databaseName}`,
+    `postgres://postgres:w2w2@localhost:5432/${dbName}`,
     {
         logging: false
     }
